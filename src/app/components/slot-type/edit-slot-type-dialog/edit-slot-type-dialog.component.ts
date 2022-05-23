8684dc57fa59966c3fca21448334a490e8c79bab
Daniel Somngesi <u19329033@tuks.co.za>
2022-05-24 01:55:28 +0200

    added slot type edit component code

--- src/app/components/slot-type/edit-slot-type-dialog/edit-slot-type-dialog.component.ts
+++ src/app/components/slot-type/edit-slot-type-dialog/edit-slot-type-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { SlotTypeService } from './../../../services/slot-type.service';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-edit-slot-type-dialog',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditSlotTypeDialogComponent implements OnInit {
 
-  constructor() { }
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  slotTypeForm: any;
+
+  constructor(public service: SlotTypeService,private _snackBar: MatSnackBar,
+    private formbulider: FormBuilder, public dialog: MatDialogRef<EditSlotTypeDialogComponent>,
+   @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
+    this.slotTypeForm = this.formbulider.group({
+      Name: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.maxLength(30)]],
+    })
+  }
+  stopEdit(): void {
+    this.service.updateItem(this.data);
+    this.SavedSuccessful(0);
+  }
+
+  onNoClick(): void {
+    this.dialog.close();
+  }
+
+  SavedSuccessful(isUpdate:any) {
+    if (isUpdate == 0) {
+      this._snackBar.open('Record Updated Successfully!', 'Close', {
+        duration: 4000,
+        horizontalPosition: this.horizontalPosition,
+        verticalPosition: this.verticalPosition,
+      });
+    }
+    else if (isUpdate == 1) {
+      this._snackBar.open('Record Saved Successfully!', 'Close', {
+        duration: 4000,
+        horizontalPosition: this.horizontalPosition,
+        verticalPosition: this.verticalPosition,
+      });
+    }
+    else if (isUpdate == 2) {
+      this._snackBar.open('Record Deleted Successfully!', 'Close', {
+        duration: 4000,
+        horizontalPosition: this.horizontalPosition,
+        verticalPosition: this.verticalPosition,
+      });
+    }
   }
 
 }
