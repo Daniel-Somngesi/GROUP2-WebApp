6d295d1e11a8400106adcac2c76e434ba9ad28f5
Abigail Shongwe <u19223758@tuks.co.za>
2022-07-31 07:29:20 +0200

    schedule service

---
+++ src/app/services/schedules/schedule.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScheduleService {
+
+  endpointBase = environment.apiUrl;
+  headers = {
+    Authorization: `Bearer ${localStorage.getItem('token')}`
+  }
+
+  constructor(private _httpClient: HttpClient) {
+  }
+
+  getAll() {
+    return this._httpClient.get(
+      this.endpointBase.concat("Schedule/All"),
+      { reportProgress: true, observe: 'events', headers: this.headers }
+    );
+  }
+
+  create(payload:any) {
+    return this._httpClient.post(
+      this.endpointBase.concat("Schedule/Create"),payload,
+      { reportProgress: true, observe: 'events', headers: this.headers }
+    );
+  }
+
+}
